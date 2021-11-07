2b50b94f63bb5dbbe015d5d210f878431ea201b8
Mustaf <mustafdiiriye@hotmail.com>
2021-11-07 19:29:13 +0200

    finished stories

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { View, Text, ScrollView } from 'react-native'
-import {USERS} from '../../data/users'
-
+import React from "react";
+import { View, Text, ScrollView, Image, StyleSheet } from "react-native";
+import { USERS } from "../../data/users";
 
 const Stories = () => {
-    return (
-  
-            <View>
-             <Text style={{color: 'white'}}>STORIES</Text>
-            <ScrollView
-              horizontal
-              showsHorizontalScrollIndicator={false}
-            >
-            
-            {USERS.map((user) => <Text style={{color: 'white', margin: 2}}>{user.name}</Text>)}
-          
-            </ScrollView>
-            
-             
-        </View>
-  
-  
-    )
-}
+  return (
+    <View style={{marginBottom: 13}}>
+      {/* <Text style={{color: 'white'}}>STORIES</Text> */}
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {USERS.map((story, index) => (
+          <View key={index} style={{alignItems: 'center'}}>
+            <Image source={{ uri: story.image }} style={styles.story} />
+            <Text style={{ color: "white", margin: 2 }}>{story.user.length > 11 ? story.user.slice(0, 10).toLowerCase() + '...' : story.user.toLowerCase()}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
-export default Stories
+export default Stories;
+
+const styles = StyleSheet.create({
+  story: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    marginLeft: 6,
+    borderWidth: 3,
+    borderColor: "#FF8501",
+  },
+});
--- data/users.js
+++ data/users.js
@@ -1,10 +1,35 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export const  USERS = [
-    
-        {name: 'mustaf', id: 1},
-        {name: 'ahmed', id: 2}
-    
-]
-
+export const USERS = [
+  {
+    id: 1,
+    user: "Mustaf",
+    image:
+      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-1/s320x320/251500216_4577582788951742_7047083087247365839_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeGkf7X2j2-vKuZwq-fSyTdPUTl1NS-80atROXU1L7zRq1qshKi4YqUry0pfKg8rlnQdP7eDVdnt51S9sbLEdkZc&_nc_ohc=hvHpfSrPrIIAX_kD2Nq&_nc_ht=scontent.famm2-3.fna&oh=ae311833ddd85cc826f55b04323a849f&oe=61AD14EF",
+  },
+  {
+    id: 2,
+    user: "Mohamed",
+    image:
+      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-9/119473969_3058487814277033_4114332802401526023_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFqHl2LNHmz-Lmrka7QbMhy6wiU3EgwudvrCJTcSDC52-pyXOHpNoTHoovs6ctKYUGj5T2-1VYUuDFInsvsAFBf&_nc_ohc=An_gsJa_UzUAX-zc8SQ&_nc_ht=scontent.famm2-3.fna&oh=0979dde575d789750c6822057bb29b8b&oe=61ABD7BC",
+  },
+    {
+    id: 4,
+    user: "Bakar",
+    image:
+      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-9/56157367_122428668922262_2375056798030757888_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHI2SUAltkz8DWENqIwSMgX8uRUOemhF-vy5FQ56aEX6_mXS_F8sJ2BFyD7Z00ZFJ0xGKP7KlWBZuF_KheTwwch&_nc_ohc=o6zrI3YryPsAX8iwKPE&_nc_ht=scontent.famm2-3.fna&oh=99b5b5776ae151fad5cf7ac30449b073&oe=61AF788F",
+  },
+  {
+    id: 3,
+    user: "Ismael_Mohamed",
+    image:
+      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-9/166765193_4219191584760278_5419838317690091680_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGnrrAyG0tXSaLxoJMpkeB-1O1kY0LZnCrU7WRjQtmcKglaSPUl4q1_hkf0M9296S0M7WMlJNR5yNdTuNeqNWvf&_nc_ohc=sbJzuP-7EdEAX92cYzb&_nc_ht=scontent.famm2-3.fna&oh=19c16b2e9d3d5fc0fbb6505b9e362659&oe=61ABC46B",
+  },
+  {
+    id: 5,
+    user: "Mustafa",
+    image:
+      "https://scontent.famm2-3.fna.fbcdn.net/v/t1.6435-9/88123663_2867214479988590_2183669726721343488_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFrcYy8zw1r3-oYO-3T-OJCbRgJIwFgdS5tGAkjAWB1LsxiD5o4TZYLh9zUwPzgPeZsp1b3LmgpAeDM4PzmKraY&_nc_ohc=HBM6mfKlSz8AX99yedU&tn=fZvDsFi2DvSZ3hrK&_nc_ht=scontent.famm2-3.fna&oh=43c4fc4d2f67b852e4166a6a832a102a&oe=61ABCE1B",
+  },
+];
