cdd8419372252637b3ce9e7641c14bd964c84755
Mustaf <mustafdiiriye@hotmail.com>
2021-11-09 16:14:22 +0200

    post footer

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,31 +1,89 @@
 import React from "react";
-import { StyleSheet, Text, View, Image} from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { Divider } from "react-native-elements";
 import { POSTS } from "./../../data/posts";
+
+const postFooterIcons = [
+  {
+    name: "Like",
+    imageUrl:
+      "https://img.icons8.com/fluency-systems-regular/452/ffffff/like--v1.png",
+    likedImageUrl: "http://",
+  },
+  {
+    name: "Comment",
+    imageUrl:
+      "https://img.icons8.com/fluency-systems-regular/344/ffffff/speech-bubble.png",
+  },
+  {
+    name: "Share",
+    imageUrl: "https://img.icons8.com/fluency-systems-regular/344/ffffff/sent.png",
+  },
+  {
+    name: "Save",
+    imageUrl: "http://",
+  },
+];
 
 const Post = ({ post }) => {
   return (
     <View style={{ marginBottom: 30 }}>
       <Divider orientation="vertical" width={1} />
       <PostHeader post={post} />
+      <PostImage post={post} />
+      <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+        <PostFooter />
+      </View>
     </View>
   );
 };
 
 const PostHeader = ({ post }) => (
-  <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5, alignItems: 'center'}}>
-    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-      <Image source={{ uri: post.profile_picture}} style={styles.story}/>
-      <Text style={{ color: "white", marginLeft: 6, fontWeight: 'bold' }}>{post.user}</Text>
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      margin: 5,
+      alignItems: "center",
+    }}
+  >
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image source={{ uri: post.profile_picture }} style={styles.story} />
+      <Text style={{ color: "white", marginLeft: 6, fontWeight: "bold" }}>
+        {post.user}
+      </Text>
     </View>
-    <Text style={{color: "white", fontWeight: "900", fontSize: 35}}>...</Text>
+    <Text style={{ color: "white", fontWeight: "900", fontSize: 35 }}>...</Text>
   </View>
+);
+
+const PostImage = ({ post }) => (
+  <View style={{ width: "100%", height: 450 }}>
+    <Image source={{ uri: post.imageUrl }} style={styles.imageStyle} />
+  </View>
+);
+
+const PostFooter = () => (
+  <View style={{flexDirection: 'row'}}>
+  <View style={styles.leftFooterIconsContainer}>
+      <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[0].imageUrl} />
+    <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[1].imageUrl} />
+    <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcons[2].imageUrl} />
+  </View>
+
+  </View>
+);
+
+const Icon = ({ imgUrl, imgStyle }) => (
+  <TouchableOpacity>
+    <Image style={imgStyle} source={{ uri: imgUrl }} />
+  </TouchableOpacity>
 );
 
 export default Post;
 
 const styles = StyleSheet.create({
-     story: {
+  story: {
     width: 40,
     height: 40,
     borderRadius: 50,
@@ -33,5 +91,19 @@ const styles = StyleSheet.create({
     borderWidth: 1.6,
     borderColor: "#FF8501",
   },
+  imageStyle: {
+    height: "100%",
+    resizeMode: "cover",
+    backgroundColor: "white",
+  },
+  footerIcon: {
+    width: 33,
+    height: 33,
+  },
+  leftFooterIconsContainer: {
+      flexDirection: "row",
+      width: "32%",
+      justifyContent: "space-between",
 
+  }
 });
--- data/posts.js
+++ data/posts.js
@@ -1,30 +1,46 @@
-import {USERS} from './users'
+import { USERS } from "./users";
 
-export const POSTS  = [
+export const POSTS = [
+  {
+    imageUrl:
+      "https://static0.srcdn.com/wordpress/wp-content/uploads/2020/10/coding1.jpg",
+    user: USERS[0].user,
+    likes: 5000,
+    caption: "good vibes",
+    profile_picture: USERS[0].image,
+    comments: [
+      { user: "Mustafa" },
+      {
+        comment: "Hi! Once I wake up, I will  finally be ready to code this up",
+      },
+      { user: "Bakar" },
+      { comment: "keep it man" },
+    ],
+  },
+  {
+    imageUrl: "https://i.ytimg.com/vi/SgqYX20Ruag/maxresdefault.jpg",
+    user: USERS[1].user,
+    likes: 7000,
+    caption: "usmle exam ",
+    profile_picture: USERS[1].image,
+    comments: [
+      { user: "Mohamed" },
+      { comment: "Medical exam" },
+      { user: "Mustaf" },
+      { comment: "all the best" },
+    ],
+  },
     {
-        imageUrl: 'http://',
-        user: USERS[0].user,
-        likes: 5000,
-        caption: "good vibes",
-        profile_picture: USERS[0].image,
-        comments: [
-            { user: 'Mustafa'},
-            {comment: 'Hi! Once I wake up, I will  finally be ready to code this up'},
-             { user: 'Bakar'},
-            {comment: 'keep it man'},
-        ]
-    },
-        {
-        imageUrl: 'http://',
-        user: USERS[1].user,
-        likes: 7000,
-        caption: "usmle exam ",
-        profile_picture: USERS[1].image,
-        comments: [
-            { user: 'Mohamed'},
-            {comment: 'Medical exam'},
-             { user: 'Mustaf'},
-            {comment: 'all the best'}
-        ]
-    }
-]
+    imageUrl: "https://p4.wallpaperbetter.com/wallpaper/787/565/98/astronaut-4k-8k-hd-wallpaper-preview.jpg",
+    user: USERS[2].user,
+    likes: 6000,
+    caption: "aestronaut",
+    profile_picture: USERS[2].image,
+    comments: [
+      { user: "Bakar" },
+      { comment: "aestronaut" },
+      { user: "Mustaf" },
+      { comment: "all the best" },
+    ],
+  },
+];
