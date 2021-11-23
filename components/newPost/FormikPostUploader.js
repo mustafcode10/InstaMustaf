f1e218f5498edf68c15cdc0d9ea9f58a967273ef
Mustaf <mustafdiiriye@hotmail.com>
2021-11-23 15:04:48 +0200

    formik post uploader

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Image } from "react-native";
 import * as Yup from "yup";
 import { Formik } from "formik";
+import { Divider } from "react-native-elements";
 
 const uploadPostSchemata = Yup.object().shape({
   imageUrl: Yup.string().url().required("A url is required"),
@@ -14,9 +15,6 @@ const PLACEHOLDER_IMG =
 const FormikPostUploader = () => {
   const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG);
   return (
-    // <View>
-    //   <Text style={{ color: "white" }}>Formik</Text>
-    // </View>
     <Formik
       initialValues={{ caption: "", imageUrl: "" }}
       onSubmit={(values) => console.log(values)}
@@ -31,13 +29,38 @@ const FormikPostUploader = () => {
         isValid,
       }) => (
         <>
-          <View>
-            <Image />
+          <View
+            style={{
+              margin: 20,
+              justifyContent: "space-between",
+              flexDirection: "row",
+            }}
+          >
+            <Image
+              source={{ uri: PLACEHOLDER_IMG }}
+              style={{ width: 100, height: 100,   }}
+            />
+            <View style={{flex: 1, marginLeft: 12,}}>
+              <TextInput
+                style={{ color: "white", fontSize: 20 }}
+                placeholder="Write a caption..."
+                placeholderTextColor="gray"
+                multiline={true}
+                onChangeText={handleChange("caption")}
+                onBlur={handleBlur("caption")}
+                value={values.caption}
+              />
+            </View>
           </View>
-          <TextInput placeholder="Write a caption"
-           placeholderTextColor="gray" />
-                     <TextInput placeholder="Enter Image Url"
-           placeholderTextColor="gray" />
+          <Divider width={0.1} orientation="vertical" />
+          <TextInput
+            style={{ color: "white", fontSize: 18 }}
+            placeholder="Enter Image Url"
+            placeholderTextColor="gray"
+            onChangeText={handleChange("imageUrl")}
+            onBlur={handleBlur("imageUrl")}
+            value={values.imageUrl}
+          />
         </>
       )}
     </Formik>
