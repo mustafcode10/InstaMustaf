0a9c3575a18c7ac68fc5845ced5e3e89f8694aaa
Mustaf <mustafdiiriye@hotmail.com>
2021-11-30 12:29:06 +0200

    SignupScreen

--- navigation.js
+++ navigation.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import NewPostScreen from "./screens/NewPostScreen";
 import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 const Stack = createStackNavigator();
 const screenOptions = {
@@ -12,10 +13,11 @@ const screenOptions = {
 
 const SignedInStack = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="HomeScreen" screenOptions={screenOptions}>
+    <Stack.Navigator initialRouteName="SignupScreen" screenOptions={screenOptions}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="NewPostScreen" component={NewPostScreen} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="SignupScreen" component={SignupScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
---
+++ screens/SignupScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+
+const INSTGRAM_LOGO =
+  "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-256.png";
+
+const SignupScreen = () => (
+  <View style={styles.container}>
+    <View style={styles.logoContainer}>
+      <Image source={{ uri: INSTGRAM_LOGO, height: 100, width: 100 }} />
+    </View>
+    {/* <LoginForm /> */}
+  </View>
+);
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 60,
+  },
+});
+
