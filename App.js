4f4e31bdc3b41cc403ffe006f9831caf184cdbe5
Mustaf <mustafdiiriye@hotmail.com>
2021-12-15 17:59:35 +0200

    AuthNavigation with firebase

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
-import NewPostScreen from './screens/NewPostScreen';
-import SignedInStack from './navigation';
+// import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+// import HomeScreen from "./screens/HomeScreen";
+// import NewPostScreen from './screens/NewPostScreen';
+// import SignedInStack from './navigation';
+import AuthNavigation from "./AuthNavigation";
 
 export default function App() {
   return (
     <>
-      {/* <StatusBar style="auto" /> */}
-      {/* <HomeScreen /> */}
-      {/* <NewPostScreen/> */}
-      <SignedInStack />
+      <AuthNavigation />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ AuthNavigation.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { SignedInStack, SignedOutStack } from "./navigation";
+import firebase from "./firebase";
+import "firebase/compat/auth";
+const auth = firebase.auth();
+
+const AuthNavigation = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const userHandler = (user) =>
+    user ? setCurrentUser(user) : setCurrentUser(null);
+  useEffect(
+    () => firebase.auth().onAuthStateChanged((user) => userHandler(user)),
+    []
+  );
+  return (
+    <>
+      {currentUser ? <SignedInStack /> : <SignedOutStack />}
+    </>
+  );
+};
+
+export default AuthNavigation;
